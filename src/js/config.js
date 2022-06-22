da940b7b3baf4719a81ae343341b028c2f3718ac
pleasantvik <adedayo.ayoola5683@gmail.com>
2022-06-22 05:17:15 +0100

    edit

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'e2930da6-b0e9-40e7-9dd4-3c763302b66f';
 export const MODAL_CLOSE_SEC = 2.5;
